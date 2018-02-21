07687e3775fedac15c0ea8ccf37cf17fc47ef5ba
Amalrajp <34888464+Amalrajp@users.noreply.github.com>
2018-02-21 21:15:24 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var thavala=document.getElementById("thavala");
 var marginLeft=1;
 function moveRight(){
-    marginLeft=marginLeft*1.001;
+    marginLeft=marginLeft*1.01;
     thavala.style.marginLeft=marginLeft+"px";
 }
 thavala.onclick=function()
