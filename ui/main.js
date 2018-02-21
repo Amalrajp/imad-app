0b4b1f35924bad189388fd55242d292357fc4d3c
Amalrajp <34888464+Amalrajp@users.noreply.github.com>
2018-02-21 21:08:18 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,12 @@
 console.log('Loaded!');
 
 var thavala=document.getElementById("thavala");
-thavala.onclick=function(){
-thavala.style.marginLeft="1000px";
+marginLeft=0;
+function moveRight(){
+    marginLeft+=marginLeft;
+    thavala.style.marginLeft=marginLeft+"px";
 }
+thavala.onclick=function()
+    {
+        var slide=setIntervel(moveRight,1);
+    };
