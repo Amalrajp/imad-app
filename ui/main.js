145f0fc13acb54662eb6717d3c7be653a2e93d3f
Amalrajp <34888464+Amalrajp@users.noreply.github.com>
2018-02-25 06:14:38 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,11 @@
 var counter=0;
 
 var btn=document.getElementById("btn");
+    var span=document.getElementById("count");
 
 btn.onclick=function()
 {  counter=counter+1;
 
-    var span=document.getElementById("count");
+
     span.innerHTML=counter;
 };
