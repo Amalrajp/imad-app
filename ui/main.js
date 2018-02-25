5f73c9ce6ec46c5c3ee2e282ace787b8c4355c9a
Amalrajp <34888464+Amalrajp@users.noreply.github.com>
2018-02-25 09:00:42 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,16 +22,30 @@ btn.onclick=function()
     request.open('GET','http://amalrajp83.imad.hasura-app.io/counter');
     request.send(null);
 };
-var nameInput=document.getElementById("name");
-var name1=nameInput.value;
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
-    var list='';
-    for(i=0;i<names.length;i++){
-        list=list+'<li>'+names[i]+'</li>';
-    }
-    var namelist=document.getElementById("namelist");
-    namelist.innerHTML=list;
+    var nameInput=document.getElementById("name");
+    var name1=nameInput.value;
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if (request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+                var names=request.responseText;
+                names=JSON.parse(names);
+                var list='';
+                for(i=0;i<names.length;i++){
+                    list=list+'<li>'+names[i]+'</li>';
+                }
+                var namelist=document.getElementById("namelist");
+                namelist.innerHTML=list;
+            }
+        }
+    };
+   
+
+    request.open('GET','http://amalrajp83.imad.hasura-app.io/submit-name?name='+name1);
+    request.send(null);
+    
 
 };
