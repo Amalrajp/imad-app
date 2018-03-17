ebb43c0d294380ff4b68fc403d3f10b24beaf0a7
Amalrajp <34888464+Amalrajp@users.noreply.github.com>
2018-03-17 12:00:55 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ submit.onclick=function(){
     };
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    console.log(username);
+    console.log(username+'main');
     request.open('POST','http://amalrajp83.imad.hasura-app.io/login',true);
     request.setRequestHeader("Content-Type",'application/json');
     request.send(JSON.stringify({"username":username,"password":password}));
